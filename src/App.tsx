2d00071256ed36b1ec265c72aecfd49c0cb8fc9d
cd10012 <ced361@nyu.edu>
2018-11-18 14:48:10 -0500

    Inital template view

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router"
 
 import Layout from "./components/Layout"
 import Home from "./components/Home"
+import Founders from "./components/Founders"
 import Notifications from "./components/Notifications"
 
 const App: SFC = () => (
   <Layout>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/configure-founders" component={Founders} />
     </Switch>
     <Notifications />
   </Layout>
