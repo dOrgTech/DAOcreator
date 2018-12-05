91a91087c597ba6bc4c578388db3f93e32981612
Asgeir Sognefest <sogasg@gmail.com>
2018-12-05 21:51:17 +0100

    Controversial refactoring
    Not really needed. Committed it to highlight some standardization challenges.

--- src/bridges/arkJs.ts
+++ src/bridges/arkJs.ts
@@ -1,12 +1,18 @@
 import { FounderConfig } from "@daostack/arc.js"
 import { BigNumber } from "bignumber.js"
+import * as R from "ramda"
 
-export const toFounderConfigs = (founders: Founder[]): Array<FounderConfig> => {
-  return founders.map(founder => {
-    return {
-      address: founder.address,
-      tokens: new BigNumber(founder.tokens),
-      reputation: new BigNumber(founder.reputation),
-    } as FounderConfig
-  })
-}
+// TODO @jordan: don't know if this refactoring is helpful or not.
+// We should decide on using array.map or R.map. I think there are a lot of
+// reasons (currying, general composability) to use the ramda package for utils like this.
+
+// Also should we use Array<Type> or Type[]
+export const toFounderConfigs = (founders: Founder[]): FounderConfig[] =>
+  R.map(
+    ({ address, tokens, reputation }) => ({
+      address,
+      tokens: new BigNumber(tokens),
+      reputation: new BigNumber(reputation),
+    }),
+    founders
+  )
