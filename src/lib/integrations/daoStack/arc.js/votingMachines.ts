b38cbcf837f28f6e5182f08beb904cb1dd420a59
Asgeir Sognefest <sogasg@gmail.com>
2018-12-18 14:58:05 +0100

    Remove exposion of integration internal type

--- src/lib/integrations/daoStack/arc.js/votingMachines.ts
+++ src/lib/integrations/daoStack/arc.js/votingMachines.ts
@@ -5,13 +5,13 @@ import {
   GenesisProtocolFactory,
 } from "@daostack/arc.js"
 
-type VotingMachineFactory = ContractWrapperFactory<IContractWrapper>
+// type VotingMachineFactory = ContractWrapperFactory<IContractWrapper>
 
-export interface VotingMachine {
+export type VotingMachine = {
   typeName: string
   // TODO: we need an address type...
   universalAddress: string
-  factory: VotingMachineFactory
+  // factory: VotingMachineFactory
   displayName: string
   description: string
 }
@@ -23,14 +23,17 @@ export const votingMachines: VotingMachine[] = [
     // for these apps... what should be registry driven and what should be baked into the app?
     // Maybe knowns baked, and new ones grabbed from registry?
     universalAddress: "0xTODO",
-    factory: AbsoluteVoteFactory,
+
+    // The factory seams like something that does not need to be exposed, like something that is internal to these
+    // integration and can be mapped from the typeName or something.
+    //factory: AbsoluteVoteFactory,
     displayName: "Absolute Vote",
     description: "Absolute Vote: useful description TODO",
   },
   {
     typeName: "GenesisProtocol",
     universalAddress: "0xTODO",
-    factory: GenesisProtocolFactory,
+    //factory: GenesisProtocolFactory,
     displayName: "Genesis Protocol",
     description: "Genesis Protocol: useful description TODO",
   },
