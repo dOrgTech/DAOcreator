1eff11e46b9a572e1d28eefff6730a14e227dfc5
jelli <jelli@dorgtech.com>
2018-12-25 18:31:40 -0800

    init unity project

---
+++ src/explorer/Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace dOrg
+{
+  public class Rotate : MonoBehaviour
+  {
+    void Update()
+    {
+      this.transform.Rotate(Vector3.up * Time.deltaTime * 10.0f);
+    }
+  }
+}
---
+++ src/explorer/README.md
@@ -0,0 +1,5 @@
+# Setup
+
+- Install Unity 2018.3.0f2
+  - Add WebGL Support
+  - Linux Instructions: https://askubuntu.com/questions/1077816/how-to-install-unity3d-on-ubuntu-18-04
