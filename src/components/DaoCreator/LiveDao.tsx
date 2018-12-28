9a42a06c31e01c89fffca77274a2fcdb2b28c239
Asgeir Sognefest <sogasg@gmail.com>
2018-12-28 14:31:12 +0100

    Add guiding info to the live DAO step

--- src/components/DaoCreator/LiveDao.tsx
+++ src/components/DaoCreator/LiveDao.tsx
@@ -34,51 +34,54 @@ const LiveDaoStep: React.SFC<Props> = ({ dao, classes }) => {
       controllerAddress,
     } = dao
     return (
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="h4" className={classes.headline} gutterBottom>
-              Live DAO
-          </Typography>
-          <Typography variant="h6">
-              The DAO is now deployed. Here are some useful information about it
-          </Typography>
-          <Grid container spacing={16}>
-            <Grid item xs={6}>
-              <Typography
-                variant="h5"
-                className={classes.headline}
-                gutterBottom
-              >
-                DAO
-              </Typography>
-              <Typography>
-                <b>Name:</b> {name}
-              </Typography>
-              <Typography>
-                <b>Token Name:</b> {tokenName}
-              </Typography>
-              <Typography>
-                <b>Token Symbol:</b> {tokenSymbol}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography
-                variant="h5"
-                className={classes.headline}
-                gutterBottom
-              >
-                Locations
-              </Typography>
-              <Typography>
-                <b>Avatar address:</b> {avatarAddress}
-              </Typography>
-              <Typography>
-                <b>Controller address:</b> {controllerAddress}
-              </Typography>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography variant="h4" className={classes.headline} gutterBottom>
+                    Live DAO
+                </Typography>
+                <Grid container spacing={16}>
+                    <Grid item xs={12} md={5}>
+                        <Typography className={classes.guideText} variant="body2">
+                            Hurray! Your DAO is deployed!
+                            <br/>
+                            Here are some useful information about it.
+                            <br />
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={7}>
+                        <Typography
+                            variant="h5"
+                            className={classes.headline}
+                            gutterBottom
+                        >
+                            DAO
+                        </Typography>
+                        <Typography>
+                            <b>Name:</b> {name}
+                        </Typography>
+                        <Typography>
+                            <b>Token Name:</b> {tokenName}
+                        </Typography>
+                        <Typography>
+                            <b>Token Symbol:</b> {tokenSymbol}
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            className={classes.headline}
+                            gutterBottom
+                        >
+                            Locations
+                        </Typography>
+                        <Typography>
+                            <b>Avatar address:</b> {avatarAddress}
+                        </Typography>
+                        <Typography>
+                            <b>Controller address:</b> {controllerAddress}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
     )
   }
 }
@@ -115,6 +118,15 @@ const styles = ({  }: Theme) =>
     daoName: {},
     tokenName: {},
     tokenSymbol: {},
+    guideText: {
+      fontSize: 18,
+      maxWidth: 450,
+      paddingLeft: 30,
+      paddingRight: 30,
+      paddingTop: 50,
+      paddingBottom: 50,
+      margin: "auto",
+    },
   })
 
 const componentWithStyles = withStyles(styles)(LiveDaoStep)
