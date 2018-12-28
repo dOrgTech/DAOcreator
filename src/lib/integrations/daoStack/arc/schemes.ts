0da6b9c5c03b9af88f43e2faf7ad946c22c2fcdf
Asgeir Sognefest <sogasg@gmail.com>
2018-12-28 10:45:19 +0100

    Remove the use of the word "scheme"
    "Scheme" is an implementation detail of DAOstack and not something that "normal people" understand

--- src/lib/integrations/daoStack/arc/schemes.ts
+++ src/lib/integrations/daoStack/arc/schemes.ts
@@ -5,57 +5,60 @@ export const schemes: Scheme[] = [
     typeName: "ContributionReward",
     displayName: "Contribution Reward",
     description:
-      "A scheme for proposing and rewarding contributions to an organization. An agent can ask to be rewarded with tokens, reputation, or any combination.",
+      "Add the possibility of proposing and rewarding contributions to an organization. An agent can ask to be rewarded with tokens, reputation, or any combination.",
     toggleDefault: true,
   },
   {
     typeName: "VoteInOrganizationScheme",
-    displayName: "Vote In Organization Scheme",
+    displayName: "Vote In Organization",
     description:
-      "A scheme that generates a new proposal where, if passed, will vote on a proposal within another DAO.",
+      "Add the possibility of creating a proposal for voting on a proposal within another DAO.",
     toggleDefault: true,
   },
   {
     typeName: "DAOCreator",
     displayName: "DAO Creator",
-    description: "A scheme that creates a new DAO.",
+    description: "Makes it possible for the DAO to create new DAOs.",
     toggleDefault: true,
   },
   {
     typeName: "SimpleICO",
     displayName: "Simple ICO",
-    description: "A scheme that holds an ICO that anyone can send funds to.",
+    description:
+      "Add the possibility of the DAO holding an Initial Coin Offering (ICO).",
     toggleDefault: false,
   },
   {
     typeName: "VestingScheme",
-    displayName: "Vesting Scheme",
-    description: "A scheme for creating a token vesting agreement.",
+    displayName: "Vesting",
+    description: "Add the possibility of creating a token vesting agreement.",
     toggleDefault: false,
   },
   {
     typeName: "OrganizationRegister",
     displayName: "Organization Register",
     description:
-      "Organizations can use this scheme to open a registry. Other organizations can then add and promote themselves on this registry.",
+      "Makes it possible for the DAO to open a registry. Other DAOs can then add and promote themselves on this registry.",
     toggleDefault: false,
   },
   {
     typeName: "UpgradeScheme",
-    displayName: "Upgrade Scheme",
-    description: "Enables the DAO to upgrade itself to a new one Controller.",
+    displayName: "Upgrade",
+    description: "Enables the DAO to upgrade itself.",
     toggleDefault: true,
   },
   {
     typeName: "SchemeRegistrar",
     displayName: "Scheme Registrar",
-    description: "Manages post-creation Scheme add/modify & removal.",
+    description:
+      " Manages post-creation adding/modifying and removing of features. Schemas are used to add features to a DAO.",
     toggleDefault: true,
   },
   {
     typeName: "GlobalConstraintRegistrar",
     displayName: "Global Constraint Registrar",
-    description: "Manages post-creation Constraint add/modify & removal.",
+    description:
+      'Makes it possible to add/modify and remove constraints for the DAO. A constraint defines what "cannot be done" in the DAO. For instance, limit the number of tokens that a DAO can create.',
     toggleDefault: true,
   },
 ]
