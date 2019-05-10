7a17f5ef83f95927e8950dcfb86f6120ff05a4a0
dOrgJelli <jelli@dorg.tech>
2019-05-10 16:59:53 -0400

    responsive landing page

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,111 +1,69 @@
 import * as React from "react"
-import { connect } from "react-redux"
-import { push } from "connected-react-router"
 import {
   withStyles,
   Typography,
   Theme,
   WithStyles,
   createStyles,
-  Card,
-  Button,
-  CardContent,
 } from "@material-ui/core"
+import InfoPage from "../common/InfoPage"
 
-// eslint-disable-next-line
-interface Props extends WithStyles<typeof styles> {
-  goHome: () => void
-}
+interface Props extends WithStyles<typeof styles> {}
 
-const About: React.SFC<Props> = ({ classes, goHome }) => (
-  <div className={classes.root}>
-    <Button
-      variant="contained"
-      size="small"
-      className={classes.button}
-      onClick={goHome}
-    >
-      Back to Home
-    </Button>
-    <div className={classes.cardWrapper}>
-      <Card className={classes.card}>
-        <CardContent className={classes.cardContent}>
-          <Typography variant="h2" className={classes.header}>
-            About dOrg
-          </Typography>
-          <Typography variant="h5">What we do</Typography>
-          <Typography variant="body1" className={classes.body}>
-            dOrg is a cooperative of freelancers building tools for distributed
-            organizations. We make ecosystem partnerships and execute on work
-            that advances the open-source DAO ecosystem.
-            <br />
-            <br />
-            For the full picture, visit our{" "}
-            <a href="https://github.com/dOrgTech/vision/blob/master/README.md">
-              project overview
-            </a>{" "}
-            on Github.
-          </Typography>
-          <Typography variant="h5">How we do it</Typography>
-          <Typography variant="body1" className={classes.body}>
-            Our freelancer co-op rests on two core technologies:
-            <li className={classes.listItem}>
-              <i>DAOstack:</i> We conduct 100% of our operations and governance
-              through <a href="https://alchemy.daostack.io">our own DAO</a>.
-            </li>
-            <li className={classes.listItem}>
-              <i>Blockchain Based LLC:</i> Our DAO is a legally registered{" "}
-              <a href="https://legislature.vermont.gov/statutes/section/11/025/04173">
-                Vermont BBLLC
-              </a>
-              .
-            </li>
-          </Typography>
-          <Typography variant="h5">Get in touch</Typography>
-          <Typography variant="body1" className={classes.body}>
-            Contact us through <a href="mailto:contact@dorg.tech">email</a> or{" "}
-            <a href="https://discord.gg/6Kujmad">Discord</a>.
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
-  </div>
+const About: React.SFC<Props> = ({ classes }) => (
+  <InfoPage
+    Content={() => (
+      <>
+        <Typography variant="h2" className={classes.header}>
+          About dOrg
+        </Typography>
+        <Typography variant="h5">What we do</Typography>
+        <Typography variant="body1" className={classes.body}>
+          dOrg is a cooperative of freelancers building tools for distributed
+          organizations. We make ecosystem partnerships and execute on work that
+          advances the open-source DAO ecosystem.
+          <br />
+          <br />
+          For the full picture, visit our{" "}
+          <a href="https://github.com/dOrgTech/vision/blob/master/README.md">
+            project overview
+          </a>{" "}
+          on Github.
+        </Typography>
+        <Typography variant="h5">How we do it</Typography>
+        <Typography variant="body1" className={classes.body}>
+          Our freelancer co-op rests on two core technologies:
+          <li className={classes.listItem}>
+            <i>DAOstack:</i> We conduct 100% of our operations and governance
+            through <a href="https://alchemy.daostack.io">our own DAO</a>.
+          </li>
+          <li className={classes.listItem}>
+            <i>Blockchain Based LLC:</i> Our DAO is a legally registered{" "}
+            <a href="https://legislature.vermont.gov/statutes/section/11/025/04173">
+              Vermont BBLLC
+            </a>
+            .
+          </li>
+        </Typography>
+        <Typography variant="h5">Get in touch</Typography>
+        <Typography variant="body1" className={classes.body}>
+          Contact us through <a href="mailto:contact@dorg.tech">email</a> or{" "}
+          <a href="https://discord.gg/6Kujmad">Discord</a>.
+        </Typography>
+      </>
+    )}
+  />
 )
 
 // STYLE
-const padding = 50
-const minWidth = 800
 const styles = (theme: Theme) =>
   createStyles({
-    root: {
-      width: "100%",
-      minWidth: minWidth + padding * 2,
-      height: "100vh",
-    },
-    cardWrapper: {
-      width: 0,
-      position: "relative",
-      left: "50%",
-      pointerEvents: "none",
-    },
-    cardContent: {},
-    card: {
-      maxWidth: 1200,
-      minWidth: minWidth,
-      position: "inherit",
-      transform: "translateX(-50%)",
-      pointerEvents: "all",
-      marginBottom: 40,
-    },
     body: {
       margin: 20,
     },
     header: {
       margin: 20,
       textAlign: "center",
-    },
-    button: {
-      margin: 10,
     },
     listItem: {
       listStyle: "decimal",
@@ -114,22 +72,4 @@ const styles = (theme: Theme) =>
     },
   })
 
-const componentWithStyles = withStyles(styles)(About)
-
-// STATE
-const mapStateToProps = (state: any) => {
-  return {}
-}
-
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    goHome: () => {
-      dispatch(push("/"))
-    },
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(componentWithStyles)
+export default withStyles(styles)(About)
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -10,6 +10,7 @@ import {
   Card,
   Button,
   CardContent,
+  Grid,
 } from "@material-ui/core"
 
 // eslint-disable-next-line
@@ -25,93 +26,99 @@ const Home: React.SFC<Props> = ({
   gotoDapp,
   gotoAbout,
 }) => (
-  <div className={classes.root}>
-    <div className={classes.topPadding} />
-    <div className={classes.cardWrapper}>
-      <Card className={classes.card}>
-        <CardContent className={classes.cardContent}>
-          <Typography variant="h2" className={classes.header}>
-            dOrg
-          </Typography>
-          <Typography variant="subtitle1" className={classes.header}>
-            Empowering Distributed Organization
-          </Typography>
-          <Button
-            variant="contained"
-            size="small"
-            className={classes.button}
-            onClick={gotoAbout}
-          >
-            About
-          </Button>
-          <Button
-            variant="contained"
-            size="small"
-            className={classes.button}
-            onClick={gotoDapp}
-          >
-            Create a DAO
-          </Button>
-          <Button
-            variant="contained"
-            size="small"
-            className={classes.button}
-            onClick={gotoOverview}
-          >
-            F.A.Q.
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
-    <div className={classes.bottomPadding} />
-  </div>
+  <Grid
+    container
+    className={classes.root}
+    alignContent="center"
+    alignItems="center"
+    direction="column"
+    justify="center"
+  >
+    <Card className={classes.card}>
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h2" className={classes.header} align="center">
+          dOrg
+        </Typography>
+        <Grid
+          container
+          className={classes.subtitleContainer}
+          direction="row"
+          justify="center"
+          spacing={8}
+        >
+          <Grid item>
+            <Typography variant="subtitle1" className={classes.header}>
+              Empowering
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" className={classes.header}>
+              Distributed
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" className={classes.header}>
+              Organizations
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container justify="center" spacing={8} direction="row">
+          <Grid key={"about"} item>
+            <Button
+              variant="contained"
+              size="small"
+              className={classes.button}
+              onClick={gotoAbout}
+            >
+              About
+            </Button>
+          </Grid>
+          <Grid key={"create"} item>
+            <Button
+              variant="contained"
+              size="small"
+              className={classes.button}
+              onClick={gotoDapp}
+            >
+              Create a DAO
+            </Button>
+          </Grid>
+          <Grid key={"faq"} item>
+            <Button
+              variant="contained"
+              size="small"
+              className={classes.button}
+              onClick={gotoOverview}
+            >
+              F.A.Q.
+            </Button>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  </Grid>
 )
 
 // STYLE
-const padding = 50
-const minWidth = 450
 const styles = (theme: Theme) =>
   createStyles({
     root: {
-      width: "100%",
-      minWidth: minWidth + padding * 2,
-      height: "100vh",
-    },
-    topPadding: {
-      width: "inherit",
-      padding: padding,
-      paddingTop: padding * 3,
-    },
-    cardWrapper: {
-      width: 0,
+      flex: 1,
+      height: "75vh",
       // bring forward (infront of background)
       position: "relative",
-      // move to horizontal middle
-      left: "50%",
       // disable pointer events, don't block background
       pointerEvents: "none",
     },
     card: {
-      maxWidth: 900,
-      minWidth: minWidth,
-      // inherit parent's position
-      position: "inherit",
-      // move horizontally left by 50% this el's width
-      transform: "translateX(-50%)",
-      // enable all pointer events
+      margin: 16,
       pointerEvents: "all",
     },
-    bottomPadding: {
-      paddingBottom: padding * 3,
-    },
-    cardContent: {
-      textAlign: "center",
-    },
-    header: {
-      margin: 10,
-    },
+    cardContent: {},
+    header: {},
+    subtitleContainer: {},
     button: {
-      margin: 10,
+      margin: 5,
     },
   })
 
