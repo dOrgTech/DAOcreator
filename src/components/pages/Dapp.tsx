e0d2dd5f9aa7ad536af8c74d7b3a9fcf2f8f3327
dOrgJelli <jelli@dorg.tech>
2019-05-10 17:07:02 -0400

    convert Dapp page

--- src/components/pages/Dapp.tsx
+++ src/components/pages/Dapp.tsx
@@ -4,92 +4,57 @@ import { push } from "connected-react-router"
 import {
   withStyles,
   Typography,
+  Button,
   Theme,
   WithStyles,
   createStyles,
-  Card,
-  Button,
-  CardContent,
 } from "@material-ui/core"
+import InfoPage from "../common/InfoPage"
 
-// eslint-disable-next-line
 interface Props extends WithStyles<typeof styles> {
   createDao: () => void
-  goHome: () => void
 }
 
-const Dapp: React.SFC<Props> = ({ classes, goHome, createDao }) => (
-  <div className={classes.root}>
-    <Button
-      variant="contained"
-      size="small"
-      className={classes.button}
-      onClick={goHome}
-    >
-      Back to Home
-    </Button>
-    <div className={classes.cardWrapper}>
-      <Card className={classes.card}>
-        <CardContent className={classes.cardContent}>
-          <Typography variant="h2" className={classes.header}>
-            Warning
-          </Typography>
-          <Typography variant="h5">Metamask Required</Typography>
-          <Typography variant="body1" className={classes.body}>
-            To interact with this application you will need the{" "}
-            <a href="https://metamask.io">Metamask browser extension</a>.
-          </Typography>
-          <Typography variant="h5">DAO Creator is in Alpha</Typography>
-          <Typography variant="body1" className={classes.body}>
-            This tool is for advanced users only. We do not advise new users
-            deploy DAOs to mainnet at this time.
-            <br />
-            To provide feedback, go{" "}
-            <a href="https://docs.google.com/forms/d/1qMwTYMFpLW0KU8l9dnHkhixwhz-fo5Qtqumjg_7JZ80">
-              here
-            </a>
-            . For any questions, reach out on{" "}
-            <a href="https://discord.gg/6Kujmad">Discord</a>!
-          </Typography>
-          <Button
-            variant="contained"
-            size="small"
-            className={classes.button}
-            onClick={createDao}
-          >
-            Proceed
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
-  </div>
+const Dapp: React.SFC<Props> = ({ classes, createDao }) => (
+  <InfoPage
+    Content={() => (
+      <>
+        <Typography variant="h2" className={classes.header}>
+          Warning
+        </Typography>
+        <Typography variant="h5">Metamask Required</Typography>
+        <Typography variant="body1" className={classes.body}>
+          To interact with this application you will need the{" "}
+          <a href="https://metamask.io">Metamask browser extension</a>.
+        </Typography>
+        <Typography variant="h5">DAO Creator is in Alpha</Typography>
+        <Typography variant="body1" className={classes.body}>
+          This tool is for advanced users only. We do not advise new users
+          deploy DAOs to mainnet at this time.
+          <br />
+          To provide feedback, go{" "}
+          <a href="https://docs.google.com/forms/d/1qMwTYMFpLW0KU8l9dnHkhixwhz-fo5Qtqumjg_7JZ80">
+            here
+          </a>
+          . For any questions, reach out on{" "}
+          <a href="https://discord.gg/6Kujmad">Discord</a>!
+        </Typography>
+        <Button
+          variant="contained"
+          size="small"
+          className={classes.button}
+          onClick={createDao}
+        >
+          Proceed
+        </Button>
+      </>
+    )}
+  />
 )
 
 // STYLE
-const padding = 50
-const minWidth = 800
 const styles = (theme: Theme) =>
   createStyles({
-    root: {
-      width: "100%",
-      minWidth: minWidth + padding * 2,
-      height: "100vh",
-    },
-    cardWrapper: {
-      width: 0,
-      position: "relative",
-      left: "50%",
-      pointerEvents: "none",
-    },
-    cardContent: {},
-    card: {
-      maxWidth: 1200,
-      minWidth: minWidth,
-      position: "inherit",
-      transform: "translateX(-50%)",
-      pointerEvents: "all",
-      marginBottom: 40,
-    },
     body: {
       margin: 20,
     },
@@ -111,9 +76,6 @@ const mapStateToProps = (state: any) => {
 
 const mapDispatchToProps = (dispatch: any) => {
   return {
-    goHome: () => {
-      dispatch(push("/"))
-    },
     createDao: () => {
       dispatch(push("/dao-creator"))
     },
