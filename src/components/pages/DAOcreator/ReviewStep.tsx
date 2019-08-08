755a94ad403730d03ad8640b893b1ec199a7f3b0
dOrgJelli <jelli@dorg.tech>
2019-08-08 19:00:08 -0400

    misc changes

--- src/components/pages/DAOcreator/ReviewStep.tsx
+++ src/components/pages/DAOcreator/ReviewStep.tsx
@@ -7,13 +7,14 @@ import {
   withStyles,
   Card,
   CardContent,
-  Typography
+  Typography,
+  Divider,
+  Grid
 } from "@material-ui/core";
 import SchemesEditor from "../../common/dao/SchemesEditor";
 import DAOConfigEditor from "../../common/dao/DAOConfigEditor";
 import MembersEditor from "../../common/dao/MembersEditor";
 import { DAOForm } from "../../../lib/forms";
-import MembersAnalytics from "../../common/dao/MembersAnalytics";
 
 interface Props extends WithStyles<typeof styles> {
   form: DAOForm;
@@ -27,22 +28,34 @@ class ReviewStep extends React.Component<Props> {
     return (
       <Card>
         <CardContent>
-          <Typography variant="h4" gutterBottom>
-            Naming
-          </Typography>
-          <DAOConfigEditor form={form.$.config} editable={false} />
-          <Typography variant="h4" gutterBottom>
-            Schemes
-          </Typography>
-          <SchemesEditor form={form.$.schemes} editable={false} />
-          <Typography variant="h4" gutterBottom>
-            Members
-          </Typography>
-          <MembersEditor
-            form={form.$.members}
-            editable={false}
-            getDAOTokenSymbol={getDAOTokenSymbol}
-          />
+          <Typography variant="h4">Review your new DAO:</Typography>
+          <Divider />
+          <Grid container spacing={3} direction={"column"}>
+            <Grid item>
+              <Typography variant="h5" gutterBottom>
+                Names
+              </Typography>
+              <Grid container justify={"center"}>
+                <DAOConfigEditor form={form.$.config} editable={false} />
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5" gutterBottom>
+                Schemes
+              </Typography>
+              <SchemesEditor form={form.$.schemes} editable={false} />
+            </Grid>
+            <Grid item>
+              <Typography variant="h5" gutterBottom>
+                Members
+              </Typography>
+              <MembersEditor
+                form={form.$.members}
+                editable={false}
+                getDAOTokenSymbol={getDAOTokenSymbol}
+              />
+            </Grid>
+          </Grid>
         </CardContent>
       </Card>
     );
