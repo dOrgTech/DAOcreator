ba1ea82b1e53ab47abba7d31a71488da73df7bae
dOrgJelli <jelli@dorg.tech>
2019-08-09 15:11:47 -0400

    fix

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-// ContextReplacementPlugin "scryptsy" -> "scryptsy/lib"
-const webpack = require("webpack");
-
-module.exports = {
-  plugins: [
-    new webpack.ContextReplacementPlugin(/scryptsy/, /scryptsy\/lib\/index.js/)
-  ]
-};
