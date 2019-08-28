bdaa91a485fd7b4b8aeeb2b792cfdc4cd1f874cf
Hector Garcia <hmgaar@gmail.com>
2019-08-27 21:14:30 -0400

    SupportButton Component created, TODO: need to change links for corresponding typeforms

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,9 @@ import { Route, Switch } from "react-router";
 
 import TopBar from "./shell/TopBar";
 import Background from "./shell/Background";
+// Note: I hate touching App.tsx file but I did not know where to render the component.
+// I can change this just let me know where shall we render the componenent.
+import { Support } from "./shell";
 import Landing from "./pages/Landing";
 import About from "./pages/About";
 import Dapp from "./pages/Dapp";
@@ -21,6 +24,7 @@ const App: SFC = () => (
   >
     <Background />
     <TopBar />
+    <Support />
     <Switch>
       <Route exact path="/" component={Landing} />
       <Route exact path="/about" component={About} />
---
+++ src/components/shell/index.ts
@@ -0,0 +1,5 @@
+// NOTE  I am proposing the idea of creating more index.ts file when it comes import/export of components.
+import Support from "./Support";
+import TopBar from "./TopBar";
+import Background from "./Background";
+export { Support, TopBar, Background };
