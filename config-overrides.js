2fb43988e61230f83ed1b798a8794b4004b98fc0
dOrgJelli <jelli@dorg.tech>
2019-10-12 15:32:33 +0300

    remove misc

--- config-overrides.js
+++ config-overrides.js
@@ -8,43 +8,4 @@ module.exports = function override(config, env) {
     }
   });
   return config;
-}; /*
-{ 
-  plugins: 
-   [ 
-     ForkTsCheckerWebpackPlugin {
-       ignoreLintWarnings: false,
-       reportFiles: [Array],
-       logger: [Object],
-       silent: true,
-       async: false,
-       checkSyntacticErrors: true,
-       resolveModuleNameModule: undefined,
-       resolveTypeReferenceDirectiveModule: undefined,
-       workersNumber: 1,
-       memoryLimit: 2048,
-       useColors: true,
-       colors: [Object],
-       formatter: [Function: formatter],
-       emitCallback: [Function: noopEmitCallback],
-       doneCallback: [Function: doneCallback],
-       typescript: [Object],
-       typescriptPath: '/home/jelli/Dev/Repos/dOrg/DAOcreator/node_modules/typescript/lib/typescript.js',
-       typescriptVersion: '3.4.5',
-       tsconfig: '/home/jelli/Dev/Repos/dOrg/DAOcreator/tsconfig.json',
-       compilerOptions: {},
-       tslintVersion: undefined,
-       vue: false,
-       useTypescriptIncrementalApi: true,
-       measureTime: false } ],
-  node: 
-   { module: 'empty',
-     dgram: 'empty',
-     dns: 'mock',
-     fs: 'empty',
-     http2: 'empty',
-     net: 'empty',
-     tls: 'empty',
-     child_process: 'empty' },
-  performance: false }
-*/
+};
