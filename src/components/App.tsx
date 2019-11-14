2cfda6ae6a539fb7bbafc26f3ed405ce559ee560
alarivan <alarivan@pm.me>
2019-11-13 16:05:02 -0800

    Remove unused pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,8 @@
 import * as React from "react";
 import { SFC } from "react";
-import { Route, Switch } from "react-router";
 
 import TopBar from "./shell/TopBar";
 import Background from "./shell/Background";
-import Landing from "./pages/Landing";
-import About from "./pages/About";
-import Dapp from "./pages/Dapp";
 import DAOcreator from "./pages/DAOcreator";
 
 const App: SFC = () => (
@@ -21,12 +17,7 @@ const App: SFC = () => (
   >
     <Background />
     <TopBar />
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/dapp" component={Dapp} />
-      <Route exact path="/dao-creator" component={DAOcreator} />
-    </Switch>
+    <DAOcreator />
   </div>
 );
 
