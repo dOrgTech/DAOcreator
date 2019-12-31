4193892c1689621a4530d2dc1b1d4d95aa1a827a
Flash <flash.jaq@gmail.com>
2019-12-31 13:27:12 +0100

    Initial work on Install Step

--- packages/ui_v2/src/components/DAOcreatorV2/InstallStep.tsx
+++ packages/ui_v2/src/components/DAOcreatorV2/InstallStep.tsx
@@ -1,16 +1,60 @@
-import * as React from "react";
-import { DAOForm } from "@dorgtech/daocreator-lib";
+import React from "react";
+import Migrator from "../commonV2/dao/Migrator";
+import {
+  DAOForm,
+  DAOMigrationResult,
+  toDAOMigrationParams
+} from "@dorgtech/daocreator-lib";
+import { MDBBox } from "mdbreact";
+
 interface Props {
-  daoForm: DAOForm;
+  form: DAOForm;
 }
 
-function InstallStep(props: Props) {
-  const printDAOForm = () => console.log("this.props.daoForm", props.daoForm);
+// interface Props extends WithStyles<typeof styles> {
+//   dao: DAOMigrationParams;
+//   onComplete: (result: DAOMigrationResult) => void;
+//   onAbort: (error: Error) => void;
+//   onStart: () => void;
+//   onStop: () => void;
+// }
+
+function InstallStep(props: any) {
+  const { form } = props;
+
+  const onComplete = () => {
+    console.log("onComplete");
+    // Update UI
+  };
+  const onStart = () => {
+    console.log("onStart");
+    // Update UI
+  };
+  const onAbort = (error: Error) => {
+    console.log(error.message);
+    onStop();
+  };
+  const onStop = () => {
+    console.log("onStop");
+    // Update UI
+  };
+
   return (
-    <>
-      <p onClick={() => printDAOForm()}>Print Form</p>
-    </>
+    <MDBBox>
+      <Migrator
+        dao={toDAOMigrationParams(form.$)}
+        onComplete={(result: DAOMigrationResult) => {
+          console.log(result);
+          onComplete();
+        }}
+        onStart={onStart}
+        onAbort={onAbort}
+        onStop={onStop}
+      />
+    </MDBBox>
   );
 }
+
+const styles = {};
 
 export default InstallStep;
