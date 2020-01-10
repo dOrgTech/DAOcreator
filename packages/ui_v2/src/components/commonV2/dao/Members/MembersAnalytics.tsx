d533e5022b046a8388c66e9991bfc9a4489a6cc4
Flash <flash.jaq@gmail.com>
2020-01-10 15:31:11 +0100

    Implemented LineGraphic

--- packages/ui_v2/src/components/commonV2/dao/Members/MembersAnalytics.tsx
+++ packages/ui_v2/src/components/commonV2/dao/Members/MembersAnalytics.tsx
@@ -2,28 +2,47 @@ import React from "react";
 import { MDBBox } from "mdbreact";
 
 import PieChart from "components/commonV2/PieChart";
+import Line from "components/commonV2/Line";
 
 export const MembersAnalytics = ({ data }: { data: any }) => {
-  const tokenDistributionConfig = {
-    size: 240,
+  // const tokenDistributionConfig = {
+  //   size: 240,
+  //   dataKey: "tokens",
+  //   nameKey: "address"
+  // };
+
+  // const reputationDistributionConfig = {
+  //   size: 240,
+  //   dataKey: "reputation",
+  //   nameKey: "address"
+  // };
+
+  const newTokenConfig = {
+    height: "10px",
+    symbol: "token",
     dataKey: "tokens",
     nameKey: "address"
   };
-  const reputationDistributionConfig = {
-    size: 240,
-    dataKey: "reputation",
+
+  const newReputationConfig = {
+    height: "10px",
+    symbol: "REP",
+    dataKey: "tokens",
     nameKey: "address"
   };
+
   const AnalyticsBoxes = () => (
     <>
       <MDBBox>
         Token Distribution
-        <PieChart data={data} config={tokenDistributionConfig} />
+        {/* <PieChart data={data} config={tokenDistributionConfig} /> */}
+        <Line data={data} config={newTokenConfig} />
       </MDBBox>
 
       <MDBBox>
         Reputation Distribution
-        <PieChart data={data} config={reputationDistributionConfig} />
+        <Line data={data} config={newReputationConfig} />
+        {/* <PieChart data={data} config={reputationDistributionConfig} /> */}
       </MDBBox>
     </>
   );
