7cfb290202ce2887646e8134cfcb91139ac868ae
Flash <flash.jaq@gmail.com>
2020-01-11 06:39:48 +0100

    Added showPercentage and tweaked styling

--- packages/ui_v2/src/components/commonV2/dao/Members/MembersAnalytics.tsx
+++ packages/ui_v2/src/components/commonV2/dao/Members/MembersAnalytics.tsx
@@ -5,6 +5,7 @@ import LineGraphic from "components/commonV2/LineGraphic";
 
 export const MembersAnalytics = ({ data }: { data: any }) => {
   const newTokenConfig = {
+    showPercentage: false,
     height: "0.5rem",
     symbol: "token", // TODO get token symbol (?)
     dataKey: "tokens",
@@ -12,6 +13,8 @@ export const MembersAnalytics = ({ data }: { data: any }) => {
   };
 
   const newReputationConfig = {
+    showPercentage: false,
+
     height: "0.5rem",
     symbol: "REP",
     dataKey: "reputation",
@@ -21,16 +24,12 @@ export const MembersAnalytics = ({ data }: { data: any }) => {
   const AnalyticsBoxes = () => (
     <>
       <MDBBox display="flex" flex="row">
-        <div style={{ whiteSpace: "nowrap", width: "10rem" }}>
-          Token Distribution
-        </div>
+        <div style={{ whiteSpace: "nowrap" }}>Token Distribution</div>
         <LineGraphic data={data} config={newTokenConfig} />
       </MDBBox>
 
       <MDBBox display="flex" flex="row">
-        <div style={{ whiteSpace: "nowrap", width: "10rem" }}>
-          Reputation Distribution
-        </div>
+        <div style={{ whiteSpace: "nowrap" }}>Reputation Distribution</div>
         <LineGraphic data={data} config={newReputationConfig} />
       </MDBBox>
     </>
