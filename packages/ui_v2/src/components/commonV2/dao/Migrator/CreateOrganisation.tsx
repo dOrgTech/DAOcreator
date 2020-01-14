11e7f47f5db9bcca60895d02a680a5d65bea06a3
Flash <flash.jaq@gmail.com>
2020-01-14 10:41:24 +0100

    Added CreateOrganisation Component

---
+++ packages/ui_v2/src/components/commonV2/dao/Migrator/CreateOrganisation.tsx
@@ -0,0 +1,31 @@
+import React, { FC, useState } from "react";
+import { MDBRow, MDBCol } from "mdbreact";
+
+interface IProps {
+  nextStep: () => void;
+}
+
+enum STEP {
+  Waiting, // Before installation
+  Start,
+  Sign,
+  View,
+  Confirmed, // Go to next step
+  Failed // Reset to start
+}
+
+export const CreateOrganisation: FC<IProps> = ({ nextStep }: IProps) => {
+  const [step, setStep] = useState(STEP.Waiting);
+
+  return (
+    <MDBRow>
+      <MDBCol size="2">ICON</MDBCol>
+      <MDBCol size="6">
+        <div>Create Organisation</div>
+      </MDBCol>
+      <MDBCol size="4">{}</MDBCol>
+    </MDBRow>
+  );
+};
+
+export default CreateOrganisation;
