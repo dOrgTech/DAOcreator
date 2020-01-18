341250862a777c6bb3cf4f2c710b6024e4879041
J Quinn <git@jaquinn.com>
2020-01-18 19:53:36 +0100

    Updated props

--- packages/ui_v2/src/components/commonV2/dao/Migrator/ConfigureOrganisation.tsx
+++ packages/ui_v2/src/components/commonV2/dao/Migrator/ConfigureOrganisation.tsx
@@ -2,19 +2,23 @@ import React, { FC, useState } from "react";
 import { MDBRow, MDBCol } from "mdbreact";
 
 interface IProps {
-  nextStep: () => void;
+  active: boolean;
+  logLines: string[];
 }
 
 enum STEP {
   Waiting, // Before installation
   Start,
-  Sign,
-  View,
+  // Sign,
+  // View, // View on Etherscan should display after tx is signed but we are not currently handling this TODO (?)
   Confirmed, // Go to next step
   Failed // Reset to start
 }
 
-export const ConfigureOrganisation: FC<IProps> = ({ nextStep }: IProps) => {
+export const ConfigureOrganisation: FC<IProps> = ({
+  active,
+  logLines
+}: IProps) => {
   const [step, setStep] = useState(STEP.Waiting);
 
   return (
