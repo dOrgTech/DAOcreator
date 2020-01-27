af28aa1381e49a94da612f336332e6a8bd7dc4cd
J Quinn <git@jaquinn.com>
2020-01-27 21:50:59 +0100

    Added loading and dependencies to useEffect hook

--- packages/ui_v2/src/components/DAOcreatorV2/SchemesStep.tsx
+++ packages/ui_v2/src/components/DAOcreatorV2/SchemesStep.tsx
@@ -20,9 +20,14 @@ interface Props {
 function SchemesStep(props: Props) {
   const { form, toggleCollapse, modal, setModal, advancedScheme } = props;
 
+  const [loading, setLoading] = React.useState(true);
+
   React.useEffect(() => {
+    if (!loading) return;
+    setLoading(false);
+
     form.$.push(new ContributionRewardForm(), new SchemeRegistrarForm());
-  }, []);
+  }, [loading, form.$]);
 
   return (
     <>
