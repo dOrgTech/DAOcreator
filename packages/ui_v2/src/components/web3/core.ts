f823a4238b4095ad05a6c3c7d2f1f3a43d8beb66
J Quinn <git@jaquinn.com>
2020-02-09 03:55:30 +0100

    Removed our existing web3 function

--- packages/ui_v2/src/components/web3/core.ts
+++ packages/ui_v2/src/components/web3/core.ts
@@ -1,27 +1,31 @@
-import Web3 from "web3";
-let web3: Web3 | undefined = undefined; // Will hold the web3 instance
+/*
+ * The following is covered under the lib
+ */
 
-export async function enableEthereum() {
-  async function getAccount() {
-    const account = await (window as any).ethereum.enable();
-    web3 = new Web3((window as any).ethereum);
-    const defaultAccount = account[0];
-    web3.eth.defaultAccount = defaultAccount;
-    return defaultAccount;
-  }
+// import Web3 from "web3";
+// let web3: Web3 | undefined = undefined; // Will hold the web3 instance
 
-  if (!web3) {
-    try {
-      // Request account access if needed
-      return getAccount();
-    } catch (error) {
-      console.log(error);
-      return undefined;
-    }
-  } else {
-    return getAccount();
-  }
-}
+// export async function enableEthereum() {
+//   async function getAccount() {
+//     const account = await (window as any).ethereum.enable();
+//     web3 = new Web3((window as any).ethereum);
+//     const defaultAccount = account[0];
+//     web3.eth.defaultAccount = defaultAccount;
+//     return defaultAccount;
+//   }
+
+//   if (!web3) {
+//     try {
+//       // Request account access if needed
+//       return getAccount();
+//     } catch (error) {
+//       console.log(error);
+//       return undefined;
+//     }
+//   } else {
+//     return getAccount();
+//   }
+// }
 
 export const handleNetworkReload = async () => {
   try {
