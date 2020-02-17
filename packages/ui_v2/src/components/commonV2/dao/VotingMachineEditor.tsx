23ad9ffe1b41737ac5c899930456a8e34c0b6ebe
J Quinn <git@jaquinn.com>
2020-02-17 09:46:19 +0100

    Working on votingMachineEditor

--- packages/ui_v2/src/components/commonV2/dao/VotingMachineEditor.tsx
+++ packages/ui_v2/src/components/commonV2/dao/VotingMachineEditor.tsx
@@ -8,10 +8,28 @@ interface Props {
   editable: boolean;
 }
 
-const VotingMachineEditor: FC<Props> = ({ fields, editable }) => (
-  <>
-    {fields.map((field: AnyField, index: number) => {
-      if (index <= 3) {
+const VotingMachineEditor: FC<Props> = ({ fields, editable }) => {
+  return (
+    <>
+      {fields.map((field: AnyField, index: number) => {
+        if (index <= 3) {
+          return (
+            <FormField
+              field={field}
+              editable={editable}
+              key={`genproto-field-${index}`}
+            />
+          );
+        } else if (index % 2 !== 0) {
+          return (
+            <MDBRow key={`genproto-field-${index}`}>
+              <FormField field={field} editable={editable} />
+              <FormField field={fields[index - 1]} editable={editable} />
+            </MDBRow>
+          );
+        }
+        // Unhandled paramNames
+        console.log("UNEXPECTED PARAM");
         return (
           <FormField
             field={field}
@@ -19,25 +37,9 @@ const VotingMachineEditor: FC<Props> = ({ fields, editable }) => (
             key={`genproto-field-${index}`}
           />
         );
-      } else if (index % 2 !== 0) {
-        return (
-          <MDBRow key={`genproto-field-${index}`}>
-            <FormField field={field} editable={editable} />
-            <FormField field={fields[index - 1]} editable={editable} />
-          </MDBRow>
-        );
-      }
-      // Unhandled paramNames
-      console.log("UNEXPECTED PARAM");
-      return (
-        <FormField
-          field={field}
-          editable={editable}
-          key={`genproto-field-${index}`}
-        />
-      );
-    })}
-  </>
-);
+      })}
+    </>
+  );
+};
 
 export default VotingMachineEditor;
