5826a42b23f5e8aefeb420726fbd522f98c924aa
mrrobot16 <hmgaar@gmail.com>
2020-02-23 19:32:03 -0500

    import module not fix, using 'require' instead of 'import'

--- packages/ui_v2/src/components/commonV2/DAOstackLogo.tsx
+++ packages/ui_v2/src/components/commonV2/DAOstackLogo.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-// This is giving an error in production mode, will look into this.
-// import logo from "../../assets/logos/dao-logo-gray.svg";
-const logo = "https://alchemy.daostack.io/assets/images/Icon/dao-logo-gray.svg";
+const logo = require("../../assets/logos/dao-logo-gray.svg");
+
 const DAOstackLogo = () => {
   return (
     <div className="text-center">
       <img style={styles.logo} src={logo} alt="dao-logo.svg" />
-      {/*<img style={styles.logo} src={logo} alt="dao-logo.svg" />*/}
       <b style={styles.text}>Powered by DAOstack</b>
     </div>
   );
