3a0568042a7941f23469ea281b56f80c78fb767a
dOrgJelli <jelli@dorg.tech>
2020-03-04 00:46:22 -0500

    version bumps + packaging fixes

--- packages/ui_v2/src/components/commonV2/DAOstackLogo.tsx
+++ packages/ui_v2/src/components/commonV2/DAOstackLogo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-const logo = require("../../assets/logos/dao-logo-gray.svg");
+const logo = require("../assets/logos/dao-logo-gray.svg");
 
 const DAOstackLogo = () => {
   return (
